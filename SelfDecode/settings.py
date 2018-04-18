62f27dcbc8874294e98c57b5767360f274740862
bnisevic <bojan.programer@gmail.com>
2018-04-18 23:58:19 +0200

    Heroku wont work with SQLite?

--- SelfDecode/settings.py
+++ SelfDecode/settings.py
@@ -12,17 +12,21 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import sys
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.insert(0, str(BASE_DIR + '/SelfDecode/sigcalc'))
 
+env = environ.Env(DEBUG=(bool, False), )
+environ.Env.read_env(BASE_DIR + '/SelfDecode/settings.env')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b5ry7^t=*nf==g9lt1#z4$jyk&aj!5kwz!vo^e1q!im5lw$d(='
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,13 +79,15 @@ WSGI_APPLICATION = 'SelfDecode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db(),  # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
