dad43241bdcd57b711388e799e5319b228b55050
bnisevic <bojan.programer@gmail.com>
2018-04-18 23:06:42 +0200

    First commit

---
+++ SelfDecode/sigcalc/views.py
@@ -0,0 +1,58 @@
+import json
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+import numpy as np
+from scipy.stats import chi2_contingency
+
+
+class SigcalcView(TemplateView):
+
+    template_name = 'sigcalc.html'
+
+    def post(self, request):
+        """
+        If request is ajax throw calculation results in JSON.
+
+        :param request:
+        :return:
+        """
+        if request.is_ajax():
+            post = request.POST
+
+            # Calculate not converted
+            notconverted = self._get_notconverted(post)
+
+            # Generate observed
+            converted_a = int(post['convertedA'])
+            converted_b = int(post['convertedB'])
+            observed = np.array([[notconverted['A'], converted_a], [notconverted['B'], converted_b]])
+
+            # Get result
+            result = chi2_contingency(observed)
+            chisq, p = result[:2]
+
+            significance = 'No'
+            if p <= 0.05:
+                significance = 'Yes'
+
+            result = {'pvalue': round(p, 2), 'significance': significance}
+
+            return HttpResponse(json.dumps(result))
+
+        return render(request, template_name='sigcalc.html')
+
+    @staticmethod
+    def _get_notconverted(post):
+        """
+        Calculate not converted for A and B
+
+        :param post: Data from the AJAX request.
+        :return:
+        """
+        notconvA = int(post['visitorsA']) - int(post['convertedA'])
+        notconvB = int(post['visitorsB']) - int(post['convertedB'])
+
+        return {'A': notconvA, 'B': notconvB}
